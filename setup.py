9e67e7ff99631349a0fbfa30ba0ae2f2c89e423d
Martin <Ho-Ro@users.noreply.github.com>
2023-08-05 19:13:49 +0200

    correct the python setup
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='adf435x',
-    packages=find_packages(),
-
-    install_requires=['pyusb>=1.0.0'],
-
-    author='Martin Homuth-Rosemann',
-    author_email='Ho-Ro@users.noreply.github.com',
-    description='A package for automating AD4350/1 boards',
-    license='GPLv3',
-    url='http://github.com/Ho-Ro/adf435x'
-)
+setup()
