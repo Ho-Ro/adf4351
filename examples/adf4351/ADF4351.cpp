06ef5e04466cb79f4df049f71e5e4332e5b982b1
Martin <Ho-Ro@users.noreply.github.com>
2024-03-04 12:33:42 +0100

    ADF parameter update
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- examples/adf4351/ADF4351.cpp
+++ examples/adf4351/ADF4351.cpp
@@ -10,8 +10,9 @@
 #include "ADF4351.h"
 
 
-void ADF435X::setFreq( uint64_t freq_Hz, uint16_t RCounter ) {
+void ADF435X::setFreq( double freq, uint16_t RCounter ) {
 
+    // start with register default values
     REG[ 5 ] = 0x00180005;
     REG[ 4 ] = 0x00000004;
     REG[ 3 ] = 0x00000003;
@@ -19,7 +20,7 @@ void ADF435X::setFreq( uint64_t freq_Hz, uint16_t RCounter ) {
     REG[ 1 ] = 0x00000001;
     REG[ 0 ] = 0x00000000;
 
-    if ( freq_Hz == 0 ) {      // switch off
+    if ( freq == 0 ) {         // switch off
         REG[ 4 ] = 0x00D01C1C; // VCO power down, MTLD, RF out disable
         return;
     }
@@ -29,13 +30,13 @@ void ADF435X::setFreq( uint64_t freq_Hz, uint16_t RCounter ) {
 
     uint16_t divider = 1;
     RF_DIV = 0;
-    for ( uint64_t f = 2200000000; f > 66000000; f /= 2 ) {
-        if ( freq_Hz >= f )
+    for ( double f = 2.2e9; f > 66e6; f /= 2 ) {
+        if ( freq >= f )
             break;
         ++RF_DIV;
         divider *= 2;
     }
-    double VCO_Freq = freq_Hz * divider;
+    double VCO_Freq = freq * divider;
     // printf( "VCO: %g GHz, divider: %d\n", VCO_Freq / 1e9, divider );
 
     INT = VCO_Freq / fPFD;
@@ -43,12 +44,10 @@ void ADF435X::setFreq( uint64_t freq_Hz, uint16_t RCounter ) {
     FRAC = MOD * ( ( (float)VCO_Freq / fPFD ) - (float)INT );
     // printf( "INT: %d, FRAC: %d, MOD: %d\n", INT, FRAC, MOD );
 
-    REG[ 5 ] |= 0x00400000; // LD = Dig LD
-    REG[ 4 ] |= 0x00801438 | ( (uint32_t)( RF_DIV ) << 20 ); // FB=fund, band_sel_clk_div=150
-    REG[ 3 ] |= 0x000004B0; // Clk Div = 150
-    REG[ 2 ] |= 0x18000E40 | RCounter << 14;
-    REG[ 1 ] |= 0x08008000 | ( (uint32_t)( MOD ) << 3 ); // 8/9, PHASE=1, CTRL=1
+    REG[ 5 ] |= 0x00400000;                                                    // LD = Dig LD
+    REG[ 4 ] |= 0x00801438 | ( (uint32_t)( RF_DIV ) << 20 );                   // FB=fund, band_sel_clk_div=150
+    REG[ 3 ] |= 0x000004B0;                                                    // Clk Div = 150
+    REG[ 2 ] |= 0x18000E40 | RCounter << 14 | ( ( FRAC ? 0b00 : 0b11 ) << 7 ); // LDF, LDP
+    REG[ 1 ] |= 0x08008000 | ( (uint32_t)( MOD ) << 3 );                       // 8/9, PHASE=1, CTRL=1
     REG[ 0 ] |= ( (uint32_t)( INT ) << 15 | (uint32_t)( FRAC ) << 3 );
 }
-
-
--- examples/adf4351/ADF4351.h
+++ examples/adf4351/ADF4351.h
@@ -11,15 +11,15 @@
 
 class ADF435X {
   public:
-    ADF435X( uint32_t reffreq = 25000000 ) : refIn{ reffreq } {};
-    void setFreq( uint64_t freq_Hz, uint16_t Rcounter = 250 );
+    ADF435X( uint32_t refIn = 25000000 ) : refIn{ refIn } {};
+    void setFreq( double freq_Hz, uint16_t Rcounter = 250 );
     uint32_t getReg( int index ) { return REG[ index ]; };
+
   private:
     uint16_t INT;
     uint16_t FRAC;
     uint16_t MOD;
     uint8_t RF_DIV;
-    // uint16_t Rcounter = 250;
     uint32_t refIn;
     uint32_t REG[ 6 ];
 };
